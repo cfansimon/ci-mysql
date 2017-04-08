aea9d0ad96a6515864fd700751c90c9997eda81e
Simon 🀇 <wq@zlzkj.com>
2017-04-08 21:00:59 +0800

    add MYSQL_DATABASE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
 #set -eo pipefail
-
+#
 sed -i "s/;*datadir\s*=.*/datadir = \/dev\/shm/g" /etc/mysql/my.cnf
 
+_get_config() {
+	local conf="$1"; shift
+	mysqld --verbose --help 2>/dev/null | awk '$1 == "'"$conf"'" { print $2; exit }'
+}
+
+echo 'Initializing database'
 mysql_install_db
+echo 'Database initialized'
 
-mysqld
+SOCKET="$(_get_config 'socket')"
 
+mysqld --skip-networking --socket="${SOCKET}" &
+pid="$!"
+
+mysql=( mysql --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" )
+
+for i in {30..0}; do
+	if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+		break
+	fi
+	echo 'MySQL init process in progress...'
+	sleep 1
+done
+if [ "$i" = 0 ]; then
+	echo >&2 'MySQL init process failed.'
+	exit 1
+fi
+
+if [ "$MYSQL_DATABASE" ]; then
+	echo "Database ${MYSQL_DATABASE} creating..."
+	echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
+fi
+
+if ! kill -s TERM "$pid" || ! wait "$pid"; then
+	echo >&2 'MySQL init process failed.'
+	exit 1
+fi
+
+echo
+echo 'MySQL init process done. Ready for start up.'
+echo
+
+exec "mysqld"
